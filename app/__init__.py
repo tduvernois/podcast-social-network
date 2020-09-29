e04c79c868039c3cc6d7c25737307e1e932294c3
tduvernois <thomas.duvernois2@gmail.com>
2020-09-28 23:07:35 -0400

    Edit db migration config

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,19 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
+
+
+from sqlalchemy import MetaData
+
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+db = SQLAlchemy(app, metadata=MetaData(naming_convention=naming_convention))
+# db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 # let flak-login knows what is the view function that handles logins
